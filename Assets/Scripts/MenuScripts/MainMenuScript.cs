bb05af42b6e7f19c0a498897e4c14f8cc1377f2d
Daniel Castellano <daniel.castellanom@gmail.com>
2017-12-12 19:06:10 -0600

    Escenas listas

---
+++ Assets/Scripts/MenuScripts/MainMenuScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void Highscore()
+    {
+        SceneManager.LoadScene("Highscore");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Music()
+    {
+
+    }
+}
